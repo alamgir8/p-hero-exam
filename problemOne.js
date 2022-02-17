c9051843ecc3669d84e6d5ad5b4e6995d7371061
alamgir8 <ahossain38900@gmail.com>
2022-02-17 16:58:29 +0600

    updated another problem

--- problemOne.js
+++ problemOne.js
@@ -16,3 +16,15 @@ console.log(name.sort());
 const rollNumbers = [12, 5, 13, 50, 100];
 
 console.log(rollNumbers.sort((a, b) => a - b));
+
+//leap year find out
+
+const isLeapYear = (year) => {
+  if (year % 400 === 0 || (year % 4 === 0 && year % 100 != 0)) {
+    console.log(`${year} is a leap!`);
+  } else {
+    console.log(`${year} not leap year!`);
+  }
+};
+
+isLeapYear(2024);
