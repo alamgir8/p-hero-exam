60d52e12f86c92d117fa46c28e1e1f67e1840080
alamgir8 <ahossain38900@gmail.com>
2022-02-23 20:30:03 +0600

    added check object has property

--- basic-data-structure.js
+++ basic-data-structure.js
@@ -120,3 +120,37 @@ const checkInventory = (scannedItem) => {
 };
 
 console.log(checkInventory('apples'));
+
+//Check if an Object has a Property
+let users = {
+  Alan: {
+    age: 27,
+    online: true,
+  },
+  Jeff: {
+    age: 32,
+    online: true,
+  },
+  Sarah: {
+    age: 48,
+    online: true,
+  },
+  Ryan: {
+    age: 19,
+    online: true,
+  },
+};
+
+const isEveryoneHere = (userObj) => {
+  // Only change code below this line
+  return (
+    userObj.hasOwnProperty('Alan') &&
+    userObj.hasOwnProperty('Jeff') &&
+    userObj.hasOwnProperty('Sarah') &&
+    userObj.hasOwnProperty('Ryan') &&
+    true
+  );
+  // Only change code above this line
+};
+
+console.log(isEveryoneHere(users));
