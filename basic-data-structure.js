fb9e8335a30bd3772ba7ee13b87ab1ed1522af1c
alamgir8 <ahossain38900@gmail.com>
2022-02-23 15:47:27 +0600

    updated with indexOf method with for loop

--- basic-data-structure.js
+++ basic-data-structure.js
@@ -46,3 +46,25 @@ const checkElement = (arr, elem) => {
 };
 
 console.log(checkElement(['squash', 'onions', 'shallots'], 'mushrooms'));
+
+//Iterate Through All an Array's Items Using For Loops
+const filteredArray = (arr, elem) => {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].indexOf(elem) === -1 && newArr.push(arr[i]);
+  }
+  return newArr;
+};
+
+console.log(
+  filteredArray(
+    [
+      [3, 2, 3],
+      [1, 6, 3],
+      [3, 13, 26],
+      [19, 2, 9],
+    ],
+    3
+  )
+);
