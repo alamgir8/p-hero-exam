8b05f2156156a0d8a6a1d3cc85e3e1f880a6f090
alamgir8 <ahossain38900@gmail.com>
2022-03-25 23:02:33 +0600

    added best cases fib function

--- fib.js
+++ fib.js
@@ -3,6 +3,18 @@ const fib = (n) => {
   return fib(n - 1) + fib(n - 2);
 };
 
-console.log(fib(6));
-console.log(fib(7));
-console.log(fib(8));
+// console.log(fib(6));
+// console.log(fib(7));
+// console.log(fib(8));
+
+//best case to use fib function
+const bestFib = (n, memo = {}) => {
+  if (n in memo) return memo[n];
+
+  if (n <= 2) return 1;
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo[n];
+};
+
+console.log(bestFib(10));
+console.log(bestFib(50));
