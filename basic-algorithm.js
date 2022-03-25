6cf0a9873c60cceb74598a08d7bcc28b5d93a369
alamgir8 <ahossain38900@gmail.com>
2022-03-25 21:39:35 +0600

    added ssecond method

--- basic-algorithm.js
+++ basic-algorithm.js
@@ -65,7 +65,18 @@ const largestNumberInArray = (arr) => {
   // return arr.map((group) => {
   //   return group.reduce((preV, currV) => (currV > preV ? currV : preV));
   // });
-  //second method  recursive approach
+  //second method
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    let largestNumber = arr[i][0];
+    for (let j = 1; j < arr[i].length; j++) {
+      if (arr[i][j] > largestNumber) {
+        largestNumber = arr[i][j];
+      }
+    }
+    result[i] = largestNumber;
+  }
+  return result;
 };
 
 console.log(
