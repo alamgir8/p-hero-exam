f813f15ee6cb810f867c9d5f91cb9899df8a20c4
alamgir8 <ahossain38900@gmail.com>
2022-03-25 21:46:54 +0600

    added new problem

--- basic-algorithm.js
+++ basic-algorithm.js
@@ -90,7 +90,8 @@ console.log(
 
 //Confirm the Ending
 const confirmEnding = (str, target) => {
-  return str;
+  //first method
+  return str.slice(str.length - target.length) === target;
 };
 
 console.log(confirmEnding('Bastian', 'n'));
