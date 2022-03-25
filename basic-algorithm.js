efca74d4de892c2292960cb58bde929f68a3bb07
alamgir8 <ahossain38900@gmail.com>
2022-03-25 21:28:26 +0600

    updated new problem

--- basic-algorithm.js
+++ basic-algorithm.js
@@ -59,3 +59,14 @@ console.log(
 );
 
 //Return Largest Numbers in Arrays
+
+const largestNumberInArray = (arr) => {};
+
+console.log(
+  largestNumberInArray([
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1],
+  ])
+);
