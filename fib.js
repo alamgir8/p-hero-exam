558ce34993dbc4968b94d3e75b459b6972991bc3
alamgir8 <ahossain38900@gmail.com>
2022-03-25 23:04:02 +0600

    updated

--- fib.js
+++ fib.js
@@ -12,7 +12,7 @@ const bestFib = (n, memo = {}) => {
   if (n in memo) return memo[n];
 
   if (n <= 2) return 1;
-  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  memo[n] = bestFib(n - 1, memo) + bestFib(n - 2, memo);
   return memo[n];
 };
 
