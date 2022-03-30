59dd080cabdf876e138ee84743b04a3b33d32895
alamgir8 <ahossain38900@gmail.com>
2022-03-31 00:11:46 +0600

    updated best case

--- dynamic-problem-solving.js
+++ dynamic-problem-solving.js
@@ -161,3 +161,8 @@ const bestSum = (targetNumber, numbers) => {
 
 console.log(bestHowSum(9, [2, 3, 6]));
 console.log(bestHowSum(300, [4, 6, 7, 14]));
+
+//doing same problem with better way
+const bestBestSum = (targetNumber, numbers, memo) => {
+  if (targetNumber in memo) return memo[targetNumber];
+};
